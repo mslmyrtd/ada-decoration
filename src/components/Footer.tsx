7f4eb7dc75f0d31373e249a46bc7be0c86e41b20
mslmyrtd <mslmyrtd@gmail.com>
2023-01-19 20:56:51 +0300

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 const Footer = () => {
     return <Wrapper>
         <h5>
-            &copy;{new Date().getFullYear()} <span>Ada Dekorasyon</span>
+            &copy;{new Date().getFullYear()} <span>ComfySloth</span>
         </h5>
         <h5> All rights reserved</h5>
     </Wrapper>
