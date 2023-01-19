077eb5a714973c101927d2b4f128456d6b5e7c9a
mslmyrtd <mslmyrtd@gmail.com>
2023-01-19 16:13:39 +0300

    created rooter in appp.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Footer, Navbar } from './components';
+import { About, Cart, Error, Home, Product, SingleProduct } from './pages';
+
 
 function App() {
-
   return (
-    <div className="App">
-      hello
-    </div>
-  )
+    <Router>
+      <Navbar />
+      <Fragment>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/products" element={<Product />} >
+            <Route path=":id" element={<SingleProduct />} />
+          </Route>
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Fragment>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
