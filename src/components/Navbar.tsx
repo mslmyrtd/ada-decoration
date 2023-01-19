91d223e2b15eb52cc7a769755efd1deacb5d8aef
mslmyrtd <mslmyrtd@gmail.com>
2023-01-19 17:44:20 +0300

    iterate link for navbar and added css property and image

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import styled from "styled-components"
 import { FaBars } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import { links } from '../utils/constant'
+import logo from '../assets/logo.svg'
+import CartButtons from './CartButtons'
 
 const Navbar = () => {
     return (
@@ -9,43 +12,89 @@ const Navbar = () => {
             <div className="nav-center">
                 <div className="nav-header">
                     <Link to="/">
-                        <p>ADA Decoration</p>
+                        <img src={logo} alt="comfy sloth" />
                     </Link>
                     <button type='button' className='nav-toggle'><FaBars /></button>
                 </div>
+                <ul className="nav-links">
+                    {links.map((link): any => {
+                        const { id, text, url } = link
+                        return <li key={id}>
+                            <Link to={url}>{text}</Link>
+                        </li>
+                    })}
+                </ul>
+                <CartButtons />
             </div>
+
         </NavContainer>
     )
 }
+
 const NavContainer = styled.nav`
-    height: 5rem;
+  height: 5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .nav-center {
+    width: 90vw;
+    margin: 0 auto;
+    max-width: var(--max-width);
+  }
+  .nav-header {
     display: flex;
     align-items: center;
-    justify-content: center;
-    .nav-center{
-        width: 90vw;
-        margin:0 auto;
-        max-width: var(--max-width);
+    justify-content: space-between;
+    img {
+      width: 175px;
+      margin-left: -15px;
     }
-    .nav-header{
-        display: flex;
-        align-items: center;
-        p{
-            width: 175px;
-            margin-left: -15px;
-        }
-    }
-    .nav-toggle{
-        background-color: transparent;
-        border: transparent;
-        color: var(--clr-primary-5);
-        cursor: pointer;
-        svg {
+  }
+  .nav-toggle {
+    background: transparent;
+    border: transparent;
+    color: var(--clr-primary-5);
+    cursor: pointer;
+    svg {
       font-size: 2rem;
     }
+  }
+  .nav-links {
+    display: none;
+  }
+  .cart-btn-wrapper {
+    display: none;
+  }
+  @media (min-width: 992px) {
+    .nav-toggle {
+      display: none;
     }
-    .nav-links{
-        display:none
+    .nav-center {
+      display: grid;
+      grid-template-columns: auto 1fr auto;
+      align-items: center;
     }
+    .nav-links {
+      display: flex;
+      justify-content: center;
+      li {
+        margin: 0 0.5rem;
+      }
+      a {
+        color: var(--clr-grey-3);
+        font-size: 1rem;
+        text-transform: capitalize;
+        letter-spacing: var(--spacing);
+        padding: 0.5rem;
+        &:hover {
+          border-bottom: 2px solid var(--clr-primary-7);
+        }
+      }
+    }
+    .cart-btn-wrapper {
+      display: grid;
+    }
+  }
 `
 export default Navbar
