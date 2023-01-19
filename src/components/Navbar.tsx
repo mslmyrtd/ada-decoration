65acd22cb78d4fc0edc4bc61d1cb03435bce365d
mslmyrtd <mslmyrtd@gmail.com>
2023-01-19 18:29:39 +0300

    navbar updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,28 +7,28 @@ import logo from '../assets/logo.svg'
 import CartButtons from './CartButtons'
 
 const Navbar = () => {
-    return (
-        <NavContainer>
-            <div className="nav-center">
-                <div className="nav-header">
-                    <Link to="/">
-                        <img src={logo} alt="comfy sloth" />
-                    </Link>
-                    <button type='button' className='nav-toggle'><FaBars /></button>
-                </div>
-                <ul className="nav-links">
-                    {links.map((link): any => {
-                        const { id, text, url } = link
-                        return <li key={id}>
-                            <Link to={url}>{text}</Link>
-                        </li>
-                    })}
-                </ul>
-                <CartButtons />
-            </div>
+  return (
+    <NavContainer>
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img src={logo} alt="comfy sloth" />
+          </Link>
+          <button type='button' className='nav-toggle'><FaBars /></button>
+        </div>
+        <ul className="nav-links">
+          {links.map((link): any => {
+            const { id, text, url } = link
+            return <li key={id}>
+              <Link to={url}>{text}</Link>
+            </li>
+          })}
+        </ul>
+        <CartButtons />
+      </div>
 
-        </NavContainer>
-    )
+    </NavContainer>
+  )
 }
 
 const NavContainer = styled.nav`
