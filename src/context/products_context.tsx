cfc79abbfff3d27ae6ff3e155232bbb7f41d3303
mslmyrtd <mslmyrtd@gmail.com>
2023-01-23 10:44:16 +0300

    product context sturcture created

---
+++ src/context/products_context.tsx
@@ -0,0 +1,36 @@
+import React, { useContext, useReducer, MouseEvent } from 'react'
+import reducer from '../reducers/products_reducer'
+
+import { InputProviderProps, ProductActionKind } from '../types/globaltypes.types'
+
+type InitialStateType = {
+    isSidebarOpen: boolean;
+    openSidebar: Function
+    closeSidebar: (event: MouseEvent<HTMLButtonElement | HTMLAnchorElement>) => void;
+}
+const initialState = {
+    isSidebarOpen: false,
+    openSidebar: () => null,
+    closeSidebar: () => null
+}
+
+const ProductsContext = React.createContext<InitialStateType>(initialState);
+
+export const ProductsProvider = ({ children }: InputProviderProps) => {
+    const [state, dispatch] = useReducer(reducer, initialState)
+    const openSidebar = () => {
+        dispatch({ type: ProductActionKind.SIDEBAR_OPEN })
+    }
+    const closeSidebar = () => {
+        dispatch({ type: ProductActionKind.SIDEBAR_CLOSE })
+    }
+    return (
+        <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar }}>
+            {children}
+        </ProductsContext.Provider>
+    )
+}
+// custom Context
+export const useProductsContext = () => {
+    return useContext(ProductsContext)
+}
