804189c40e829aab1d2714f2ddd85d1ff978920e
mslmyrtd <mslmyrtd@gmail.com>
2023-01-23 12:23:34 +0300

    added some export

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,18 @@ import Navbar from './Navbar'
 import Footer from './Footer'
 import Sidebar from './Sidebar'
 import PageHero from './PageHero'
+import FeaturedProducts from './FeaturedProducts'
+import Hero from './Hero'
+import Services from './Services'
+import Contact from './Contact'
 
-export { Navbar, Footer, Sidebar, PageHero }
+export {
+  Navbar,
+  Footer,
+  Sidebar,
+  PageHero,
+  FeaturedProducts,
+  Hero,
+  Services,
+  Contact,
+}
