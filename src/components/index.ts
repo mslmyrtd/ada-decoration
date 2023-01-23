5e16e660c18610970c8e1d6e242e9e1f2cdecc4d
mslmyrtd <mslmyrtd@gmail.com>
2023-01-23 11:54:39 +0300

    export PageHero

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import Navbar from './Navbar'
 import Footer from './Footer'
 import Sidebar from './Sidebar'
+import PageHero from './PageHero'
 
-export { Navbar, Footer, Sidebar }
+export { Navbar, Footer, Sidebar, PageHero }
