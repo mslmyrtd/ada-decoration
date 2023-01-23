4720d8c05f07a87942239d259c5683980faaaf60
mslmyrtd <mslmyrtd@gmail.com>
2023-01-23 11:14:55 +0300

    added erorr pegae structure

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
-
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 const ErrorPage = () => {
-    return (
-        <div>
-            erro
-        </div>
-    )
+    return <Wrapper>error page</Wrapper>
 }
+
+const Wrapper = styled.main`
+  background: var(--clr-primary-10);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  h1 {
+    font-size: 10rem;
+  }
+  h3 {
+    text-transform: none;
+    margin-bottom: 2rem;
+  }
+`
 
 export default ErrorPage
