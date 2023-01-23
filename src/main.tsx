cc57910de7f80f8cbda0ce3784757c63fed7963f
mslmyrtd <mslmyrtd@gmail.com>
2023-01-23 10:43:50 +0300

    app wrapped with ProductProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { ProductsProvider } from './context/products_context'
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ProductsProvider>
+      <App />
+    </ProductsProvider>
   </React.StrictMode>,
 )
