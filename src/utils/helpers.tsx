5162a1c26ee6bc603d6dbc1b18fd4b0319f2a11a
mslmyrtd <mslmyrtd@gmail.com>
2023-01-25 14:53:29 +0300

    updated

--- src/utils/helpers.tsx
+++ src/utils/helpers.tsx
@@ -1,8 +1,7 @@
 export const formatPrice = (number: number) => {
-    const newNumber = Intl.NumberFormat("en-US", {
+    return new Intl.NumberFormat("en-US", {
         style: "currency",
         currency: "USD"
     }).format(number / 100)
-    return newNumber
 }
 
