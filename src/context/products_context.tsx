ceca6a23102db315e71f087911bbb66f0ad1371f
mslmyrtd <mslmyrtd@gmail.com>
2023-01-25 15:58:34 +0300

    added new actions for fetch single product and function

--- src/context/products_context.tsx
+++ src/context/products_context.tsx
@@ -8,10 +8,13 @@ type InitialStateType = {
     openSidebar: (event: MouseEvent<HTMLButtonElement | HTMLAnchorElement | HTMLButtonElement>) => void;
     closeSidebar: (event: MouseEvent<HTMLButtonElement | HTMLAnchorElement | HTMLButtonElement>) => void;
     products_loading: boolean;
-    featured_products: Array<any>
+    featured_products: Record<string, unknown>[]
     products_error: boolean
-    products: Array<any>
+    products: Record<string, unknown>[]
     isSidebarOpen: boolean;
+    single_product_loading: boolean
+    single_product_error: boolean
+    single_product: null | object
 }
 
 const initialState = {
@@ -21,7 +24,10 @@ const initialState = {
     products_loading: false,
     featured_products: [],
     products_error: false,
-    products: []
+    products: [],
+    single_product_loading: false,
+    single_product_error: false,
+    single_product: null
 }
 
 const ProductsContext = React.createContext<InitialStateType>(initialState);
@@ -38,12 +44,24 @@ export const ProductsProvider = ({ children }: InputProviderProps) => {
         dispatch({ type: ProductActionKind.GET_PRODUCTS_BEGIN })
         try {
             const response = await axios(url)
-            const products = response.data
+            const products = response.data as Record<string, unknown>[]
             dispatch({ type: ProductActionKind.GET_PRODUCTS_SUCCESS, payload: products })
         } catch (error) {
             dispatch({ type: ProductActionKind.GET_PRODUCTS_ERROR })
 
         }
+    }
+
+    const fetchSingleProduct = async (url: string) => {
+        dispatch({ type: ProductActionKind.GET_SINGLE_PRODUCT_BEGIN })
+        try {
+            const response = await axios(url)
+            const singleProduct = response.data;
+            dispatch({ type: ProductActionKind.GET_SINGLE_PRODUCT_SUCCESS, payload: singleProduct })
+        } catch (error) {
+            dispatch({ type: ProductActionKind.GET_SINGLE_PRODUCT_ERROR })
+        }
+
     }
     useEffect(() => {
         fetchProducts(url)
