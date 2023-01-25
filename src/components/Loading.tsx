10fe2a2f533e43996d0ffb42493ec3cc4b514378
mslmyrtd <mslmyrtd@gmail.com>
2023-01-25 14:09:11 +0300

    loading.tsx created

---
+++ src/components/Loading.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Loading = () => {
+    return (
+        <div className='section section-center'>
+            <div className="loading">
+
+            </div>
+        </div>
+    )
+}
+
+export default Loading
