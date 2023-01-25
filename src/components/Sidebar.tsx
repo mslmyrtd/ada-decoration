3761aa748a8228b5fda89e2dcc420f80e3810ac8
mslmyrtd <mslmyrtd@gmail.com>
2023-01-25 15:59:12 +0300

    added new type globaltyeps

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -10,7 +10,6 @@ import { useProductsContext } from '../context/products_context'
 
 const Sidebar = () => {
   const { isSidebarOpen, closeSidebar } = useProductsContext()
-  console.log(isSidebarOpen);
   return <SidebarContainer>
     <aside className={`${isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}`}>
       <div className="sidebar-header">
--- src/types/globaltypes.types.ts
+++ src/types/globaltypes.types.ts
@@ -20,8 +20,11 @@ export enum ProductActionKind {
   GET_PRODUCTS_BEGIN = 'GET_PRODUCTS_BEGIN',
   GET_PRODUCTS_SUCCESS = 'GET_PRODUCTS_SUCCESS',
   GET_PRODUCTS_ERROR = 'GET_PRODUCTS_ERROR',
+  GET_SINGLE_PRODUCT_BEGIN = 'GET_SINGLE_PRODUCT_BEGIN',
+  GET_SINGLE_PRODUCT_SUCCESS = 'GET_SINGLE_PRODUCT_SUCCESS',
+  GET_SINGLE_PRODUCT_ERROR = 'GET_SINGLE_PRODUCT_ERROR',
 }
 export interface ProductAction {
   type: ProductActionKind
-  payload?: number | string
+  payload?: number | string | Record<string, unknown>[]
 }
