bbe22235c30cfab1fa6dd8ce2fb42d388f80128a
mslmyrtd <mslmyrtd@gmail.com>
2023-01-26 19:05:54 +0300

    for prdoduct shown product name after home

--- src/components/PageHero.tsx
+++ src/components/PageHero.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 type HeroProps = {
-  title: string
+  title: string,
+  productName?: string
 }
-const PageHero = ({ title }: HeroProps) => {
+const PageHero = ({ title, productName }: HeroProps) => {
   return <Wrapper>
     <div className="section-center">
       <h3>
-        <Link to="/">Home</Link>/{title}
+        <Link to="/">Home</Link>
+        {productName && <Link to="/products">/Products</Link>}
+        /{title}
       </h3>
     </div>
   </Wrapper>
