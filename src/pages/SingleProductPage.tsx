7c2fce4b515fe09b4b8ba1d6ee3ff6b35058284b
mslmyrtd <mslmyrtd@gmail.com>
2023-01-26 11:49:46 +0300

    singleProductPage structure created and used useParams and global context for  frtch dingleproduct

--- src/pages/SingleProductPage.tsx
+++ src/pages/SingleProductPage.tsx
@@ -1,12 +1,69 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import { useProductsContext } from '../context/products_context'
+import { single_product_url as url } from '../utils/constants'
+import {
+    Loading,
+    Error,
+} from '../components'
+import styled from 'styled-components'
 
 const SingleProductPage = () => {
-    return (
-        <div>
-            Hellooooo
-
-        </div>
-    )
+    const { id } = useParams();
+    const navigate = useNavigate()
+    console.log(id);
+    const { fetchSingleProduct, single_product_loading: loading, single_product_error: error, single_product: product } = useProductsContext()
+    useEffect(() => {
+        fetchSingleProduct(`${url}${id}`)
+    }, [id])
+    useEffect(() => {
+        if (error) {
+            setTimeout(() => {
+                navigate("/")
+            }, 3000);
+        }
+    }, [error])
+    if (loading) {
+        return <Loading />
+    }
+    if (error) {
+        return <Error />
+    }
+    return <h4>single product pag</h4>
 }
+
+const Wrapper = styled.main`
+  .product-center {
+    display: grid;
+    gap: 4rem;
+    margin-top: 2rem;
+  }
+  .price {
+    color: var(--clr-primary-5);
+  }
+  .desc {
+    line-height: 2;
+    max-width: 45em;
+  }
+  .info {
+    text-transform: capitalize;
+    width: 300px;
+    display: grid;
+    grid-template-columns: 125px 1fr;
+    span {
+      font-weight: 700;
+    }
+  }
+
+  @media (min-width: 992px) {
+    .product-center {
+      grid-template-columns: 1fr 1fr;
+      align-items: center;
+    }
+    .price {
+      font-size: 1.25rem;
+    }
+  }
+`
 
 export default SingleProductPage
