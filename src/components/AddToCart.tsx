316e52e571080b518b97ce6c8dbd3bd8e960375f
mslmyrtd <mslmyrtd@gmail.com>
2023-02-01 14:10:01 +0300

    AddTocart comp new comp added Amount buttons and send props amount,increase... and button added

--- src/components/AddToCart.tsx
+++ src/components/AddToCart.tsx
@@ -3,34 +3,54 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import { FaCheck } from 'react-icons/fa'
 import { IProduct } from '../types/globaltypes.types'
-// import { useCartContext } from '../context/cart_context'
-// import AmountButtons from './AmountButtons'
+import AmountButtons from './AmountButtons'
 
 interface IProps {
-    product: IProduct
+  product: IProduct
 }
 
 
 const AddToCart = ({ product }: IProps) => {
-    console.log(product);
-    const { id, stock, colors } = product
-    const [mainColor, setMainColor] = useState(colors[0])
+  const { id, stock, colors } = product
+  const [mainColor, setMainColor] = useState(colors[0])
+  const [amount, setAmount] = useState(1)
 
-    return (
-        <Wrapper>
-            <div className='colors'>
-                <span>colors:</span>
-                <div>
-                    {colors.map((color, index) => {
-                        return <button key={index} style={{ background: color }} onClick={() => setMainColor(color)} className={
-                            `${mainColor === color ? "color-btn active" : "color-btn"}`
-                        } >{mainColor === color ? <FaCheck /> : null}</button>
-                    })}
-                </div>
-            </div>
-            <div className="btn-container"></div>
-        </Wrapper>
-    )
+  const increase = (): void => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount + 1
+      if (tempAmount > stock) {
+        tempAmount = stock
+      }
+      return tempAmount
+    })
+  }
+  const decrease = (): void => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount - 1
+      if (tempAmount < 1) {
+        tempAmount = 1
+      }
+      return tempAmount
+    })
+  }
+  return (
+    <Wrapper>
+      <div className='colors'>
+        <span>colors:</span>
+        <div>
+          {colors.map((color, index) => {
+            return <button key={index} style={{ background: color }} onClick={() => setMainColor(color)} className={
+              `${mainColor === color ? "color-btn active" : "color-btn"}`
+            } >{mainColor === color ? <FaCheck /> : null}</button>
+          })}
+        </div>
+      </div>
+      <div className="btn-container">
+        <AmountButtons amount={amount} increase={increase} decrease={decrease} />
+        <Link to="/cart" className="btn">add to cart</Link>
+      </div>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`
