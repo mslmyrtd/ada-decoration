c2b8e7cc2f34025826b027360a1af750bc4588cb
mslmyrtd <mslmyrtd@gmail.com>
2023-02-01 11:52:35 +0300

    Stars comp structure created and writed logic to show stars and reviews

--- src/components/Stars.tsx
+++ src/components/Stars.tsx
@@ -1,11 +1,43 @@
 import React from 'react'
+import styled from 'styled-components'
+import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
 
-const Stars = () => {
-    return (
-        <div>
 
-        </div>
-    )
+interface IProps {
+    stars: number,
+    reviews?: number
 }
 
+
+const Stars = ({ stars, reviews }: IProps) => {
+    const tempStars = Array.from({ length: 5 }, (_: undefined, index: number) => {
+        const number = index + 0.5
+        return (<span key={index}>
+            {
+                stars >= index + 1 ? (<BsStarFill />) : stars >= number ? (<BsStarHalf />) : (<BsStar />)
+            }
+        </span>)
+    })
+    return <Wrapper>
+        <div className="stars">
+            {tempStars}
+        </div>
+        <p className="reviews">({reviews} customer reviews)</p>
+    </Wrapper>
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  span {
+    color: #ffb900;
+    font-size: 1rem;
+    margin-right: 0.25rem;
+  }
+  p {
+    margin-left: 0.5rem;
+    margin-bottom: 0;
+  }
+  margin-bottom: 0.5rem;
+`
 export default Stars
