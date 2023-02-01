5ff4315ba540354d3d2f83811d27b7f774d24f36
mslmyrtd <mslmyrtd@gmail.com>
2023-02-01 14:11:02 +0300

    created comp of AmountButtons and styled comp and come as a props used in ui

---
+++ src/components/AmountButtons.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import styled from 'styled-components'
+import { FaPlus, FaMinus } from 'react-icons/fa'
+
+interface IAmount {
+    amount: number,
+    increase: () => void,
+    decrease: () => void,
+
+}
+
+
+const AmountButtons = ({ amount, increase, decrease }: IAmount) => {
+    return <Wrapper className='amount-btns'>
+        <button type='button' className='amount-btn' onClick={decrease}><FaMinus /></button>
+        <h2 className='amount'>{amount}</h2>
+        <button type='button' className='amount-btn' onClick={increase}><FaPlus /></button>
+    </Wrapper>
+}
+
+const Wrapper = styled.div`
+  display: grid;
+  width: 140px;
+  justify-items: center;
+  grid-template-columns: repeat(3, 1fr);
+  align-items: center;
+  h2 {
+    margin-bottom: 0;
+  }
+  button {
+    background: transparent;
+    border-color: transparent;
+    cursor: pointer;
+    padding: 1rem 0;
+    width: 2rem;
+    height: 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  h2 {
+    margin-bottom: 0;
+  }
+`
+
+export default AmountButtons
