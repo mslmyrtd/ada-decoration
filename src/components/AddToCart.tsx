17b03f8ac571f9ce5074c7ced0eade407aced67a
mslmyrtd <mslmyrtd@gmail.com>
2023-02-01 12:27:58 +0300

    AddToCart comp structure created and added logic

--- src/components/AddToCart.tsx
+++ src/components/AddToCart.tsx
@@ -1,11 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import { FaCheck } from 'react-icons/fa'
+import { IProduct } from '../types/globaltypes.types'
+// import { useCartContext } from '../context/cart_context'
+// import AmountButtons from './AmountButtons'
 
-const AddToCart = () => {
+interface IProps {
+    product: IProduct
+}
+
+
+const AddToCart = ({ product }: IProps) => {
+    console.log(product);
+    const { id, stock, colors } = product
+    const [mainColor, setMainColor] = useState(colors[0])
+
     return (
-        <div>
-
-        </div>
+        <Wrapper>
+            <div className='colors'>
+                <span>colors:</span>
+                <div>
+                    {colors.map((color, index) => {
+                        return <button key={index} style={{ background: color }} onClick={() => setMainColor(color)} className={
+                            `${mainColor === color ? "color-btn active" : "color-btn"}`
+                        } >{mainColor === color ? <FaCheck /> : null}</button>
+                    })}
+                </div>
+            </div>
+            <div className="btn-container"></div>
+        </Wrapper>
     )
 }
 
+const Wrapper = styled.section`
+  margin-top: 2rem;
+  .colors {
+    display: grid;
+    grid-template-columns: 125px 1fr;
+    align-items: center;
+    margin-bottom: 1rem;
+    span {
+      text-transform: capitalize;
+      font-weight: 700;
+    }
+    div {
+      display: flex;
+    }
+  }
+  .color-btn {
+    display: inline-block;
+    width: 1.5rem;
+    height: 1.5rem;
+    border-radius: 50%;
+    background: #222;
+    margin-right: 0.5rem;
+    border: none;
+    cursor: pointer;
+    opacity: 0.5;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    svg {
+      font-size: 0.75rem;
+      color: var(--clr-white);
+    }
+  }
+  .active {
+    opacity: 1;
+  }
+  .btn-container {
+    margin-top: 2rem;
+  }
+
+  .btn {
+    margin-top: 1rem;
+    width: 140px;
+  }
+`
 export default AddToCart
