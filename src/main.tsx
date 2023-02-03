91b45089c4bca50afd3c287d507a1f57db7942f5
mslmyrtd <mslmyrtd@gmail.com>
2023-02-03 14:12:30 +0300

    FilterProvide added to main

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { ProductsProvider } from './context/products_context'
+import { FilterProvider } from './context/filter_context'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ProductsProvider>
-      <App />
+      <FilterProvider>
+        <App />
+      </FilterProvider>
     </ProductsProvider>
   </React.StrictMode>,
 )
