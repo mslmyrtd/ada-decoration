8bb2b85a9f79766d818023a047c1ddbf5c22d1ef
mslmyrtd <mslmyrtd@gmail.com>
2023-02-03 17:31:18 +0300

    added grid_view and show no product when user filter and  no prdoucts come

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -5,11 +5,17 @@ import { useFilterContext } from '../context/filter_context'
 
 
 const ProductList = () => {
-    const { filtered_products: products } = useFilterContext()
+    const { filtered_products: products, grid_view } = useFilterContext()
+    if (products.length < 1) {
+        return <h5 style={{ textTransform: "none" }} >
+            Sorry, no products matched your search...
+        </h5>
+    }
+    if (grid_view === false) {
+        return <ListView products={products} />
+    }
     return (
         <GridView products={products} />
-
-
     )
 }
 
