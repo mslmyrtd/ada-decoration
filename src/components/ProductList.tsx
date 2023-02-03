60a39dc5e3c688b7cde5855ab8401524919b3738
mslmyrtd <mslmyrtd@gmail.com>
2023-02-03 16:30:25 +0300

    products send as a prop to gridwiev

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import GridView from './GridView'
+import ListView from './ListView'
+import { useFilterContext } from '../context/filter_context'
+
 
 const ProductList = () => {
+    const { filtered_products: products } = useFilterContext()
     return (
-        <div>
-            ProductList
-        </div>
+        <GridView products={products} />
+
+
     )
 }
 
