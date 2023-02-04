4548c452f1926673ac1dd4936de34acc779343d1
mslmyrtd <mslmyrtd@gmail.com>
2023-02-04 13:46:48 +0300

    getUniqueValues func added to helpers for filtyering for colors, company and category

--- src/utils/helpers.tsx
+++ src/utils/helpers.tsx
@@ -1,7 +1,16 @@
+import { IProduct } from "../types/globaltypes.types"
+
 export const formatPrice = (number: number) => {
     return new Intl.NumberFormat("en-US", {
         style: "currency",
         currency: "USD"
     }).format(number / 100)
+}
+export const getUniqueValues = (data: any, type: string) => {
+    let unique = data.map((item: any) => item[type])
+    if (type == "colors") {
+        unique = unique.flat()
+    }
+    return ["all", ...new Set(unique)]
 }
 
