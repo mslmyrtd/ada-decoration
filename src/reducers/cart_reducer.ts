d65f9fbdf4b682bd48af01b952c6acf7becad547
mslmyrtd <mslmyrtd@gmail.com>
2023-02-09 16:17:45 +0300

    added add_to_cart method for add item

--- src/reducers/cart_reducer.ts
+++ src/reducers/cart_reducer.ts
@@ -1,5 +1,30 @@
-const cart_reducer = (state, action) => {
-  return state
+import { FilterActionKind, FilterAction } from '../types/globaltypes.types'
+import { InitialStateType } from '../context/cart_context'
+
+const cart_reducer = (state: InitialStateType, action: FilterAction) => {
+  const { type, payload } = action
+  switch (type) {
+    case FilterActionKind.ADD_TO_CART:
+      const { id, color, amount, product } = payload
+      console.log(id, color, amount, product)
+      const tempItem = state.cart.find((i) => i.id === id + color)
+      if (tempItem) {
+      } else {
+        const newItem = {
+          id: id + color,
+          name: product.name,
+          color,
+          amount,
+          image: product.images[0].url,
+          price: product.price,
+          max: product.stock,
+        }
+        return { ...state, cart: [...state.cart, newItem] }
+      }
+      return {
+        ...state,
+      }
+  }
   throw new Error(`No Matching "${action.type}" - action type`)
 }
 
