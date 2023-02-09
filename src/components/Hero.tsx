cb8bc635650e6b0e1110b970219851b31f03582d
mslmyrtd <mslmyrtd@gmail.com>
2023-02-09 16:17:56 +0300

    bug fixed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,18 +5,18 @@ import heroBcg from '../assets/hero-bcg.jpeg'
 import heroBcg2 from '../assets/hero-bcg-2.jpeg'
 
 const Hero = () => {
-    return <Wrapper className='section-center'>
-        <article className='content'>
-            <h1>design your <br />
-                comfort zone </h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, praesentium corrupti delectus maxime perspiciatis qui vel aspernatur incidunt earum laborum ipsum in consequuntur quas, sint quidem dolorum nobis necessitatibus. Velit.</p>
-            <Link to="/product" className='btn hero-btn'> shop now</Link>
-        </article>
-        <article className='img-container'>
-            <img src={heroBcg} alt="nice table" className='main-img' />
-            <img src={heroBcg2} alt="person working" className='accent-img' />
-        </article>
-    </Wrapper>
+  return <Wrapper className='section-center'>
+    <article className='content'>
+      <h1>design your <br />
+        comfort zone </h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, praesentium corrupti delectus maxime perspiciatis qui vel aspernatur incidunt earum laborum ipsum in consequuntur quas, sint quidem dolorum nobis necessitatibus. Velit.</p>
+      <Link to="/products" className='btn hero-btn'> shop now</Link>
+    </article>
+    <article className='img-container'>
+      <img src={heroBcg} alt="nice table" className='main-img' />
+      <img src={heroBcg2} alt="person working" className='accent-img' />
+    </article>
+  </Wrapper>
 }
 
 const Wrapper = styled.section`
