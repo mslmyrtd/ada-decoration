b7283034e04589e983abcd8cdf04b1ee2d42db35
mslmyrtd <mslmyrtd@gmail.com>
2023-02-10 14:02:07 +0300

    updated

--- src/components/CartContent.tsx
+++ src/components/CartContent.tsx
@@ -8,20 +8,29 @@ import CartTotals from './CartTotals'
 
 const CartContent = () => {
     const { cart, clearCart } = useCartContext()
-    return <Wrapper className='section section-center'>
-        <CartColumns />
-        {
-            cart.map((item) => {
+    return (
+        <Wrapper className='section section-center'>
+            <CartColumns />
+            {cart.map((item) => {
                 return <CartItem key={item.id} {...item} />
-            })
-        }
-        <hr />
-        <div className="link-container">
-            <Link to="/products" className='link-btn'> continue shopping</Link>
-            <button type='button' className='link-btn clear-btn' onClick={clearCart}>clear shopping cart</button>
-        </div>
-        <CartTotals />
-    </Wrapper>
+            })}
+            <hr />
+            <div className='link-container'>
+                <Link to='/products' className='link-btn'>
+                    {' '}
+                    continue shopping
+                </Link>
+                <button
+                    type='button'
+                    className='link-btn clear-btn'
+                    onClick={clearCart}
+                >
+                    clear shopping cart
+                </button>
+            </div>
+            <CartTotals />
+        </Wrapper>
+    )
 }
 const Wrapper = styled.section`
   .link-container {
