67ad0a2fc7d4f66643808cf1214a7c97b63a46f0
mslmyrtd <mslmyrtd@gmail.com>
2023-02-11 01:34:02 +0300

    totalitems showed dynamically

--- src/components/CartButtons.tsx
+++ src/components/CartButtons.tsx
@@ -1,16 +1,18 @@
 import { FaShoppingCart, FaUserPlus } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+import { useCartContext } from '../context/cart_context'
 import { useProductsContext } from '../context/products_context'
 
 const CartButtons = () => {
   const { closeSidebar } = useProductsContext()
+  const { total_items } = useCartContext()
 
   return (
     <Wrapper className='cart-btn-wrapper'>
       <Link to="/cart" className='cart-btn' onClick={closeSidebar}>
         Cart <span className='cart-container'><FaShoppingCart />
-          <span className='cart-value'>15</span>
+          <span className='cart-value'>{total_items}</span>
         </span>
       </Link>
       <button type='button' className='auth-btn'>
