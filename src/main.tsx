12da0364f04e4184583a0e33dc6f7aa48903b9d5
mslmyrtd <mslmyrtd@gmail.com>
2023-02-13 10:37:51 +0300

    Auth0Provider added to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,28 @@ import './index.css'
 import { ProductsProvider } from './context/products_context'
 import { FilterProvider } from './context/filter_context'
 import { CartProvider } from './context/cart_context'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <ProductsProvider>
-      <FilterProvider>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </FilterProvider>
-    </ProductsProvider>
-  </React.StrictMode>,
+  <Auth0Provider
+    domain="dev-1ryx8xldxiclf78v.us.auth0.com"
+    clientId="ep3aHsElcscPIIymG85dAVvJKTVgOoKH"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+    cacheLocation="localstorage"
+  >
+    <React.StrictMode>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </React.StrictMode>
+
+  </Auth0Provider>,
+
 )
