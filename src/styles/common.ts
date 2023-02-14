7d724d6ce5f7d3a9934dafae9e1f008d922435b0
mslmyrtd <mslmyrtd@gmail.com>
2023-02-14 15:20:28 +0300

    some chnages

--- src/styles/common.ts
+++ src/styles/common.ts
@@ -2,15 +2,14 @@ import styled from 'styled-components'
 
 export const Wrapper = styled.div`
   width: 100%;
+  min-height: 80vh;
   display: flex;
   flex-direction: column;
   align-items: center;
 
   .header {
     font-size: 20px;
-    font-family: 'Open Sans', sans-serif;
   }
-
   .content {
     display: flex;
     flex-direction: column;
@@ -22,9 +21,8 @@ export const Wrapper = styled.div`
         height: 100%;
       }
     }
-
     .form {
-      margin-top: 2em;
+      margin-top: 1.5em;
       display: flex;
       flex-direction: column;
       align-items: center;
@@ -35,7 +33,10 @@ export const Wrapper = styled.div`
         align-items: flex-start;
         width: fit-content;
         label {
-          font-size: 20px;
+          color: var(--clr-grey-3);
+          font-size: 1rem;
+          text-transform: capitalize;
+          letter-spacing: var(--spacing);
         }
         input {
           margin-top: 6px;
@@ -50,13 +51,16 @@ export const Wrapper = styled.div`
           margin-bottom: 31px;
           transition: all 250ms ease-in-out;
           &:hover {
-            //background-color: #ffffff;
-            //box-shadow: 0px 0px 14px 0.3px #0e81ce96;
+            background-color: #ffffff;
+            box-shadow: 0px 0px 8px 0.3px #0e81ce96;
           }
 
           &:focus {
             outline: none;
-            box-shadow: 0px 0px 8px 0.8px #3474dbb2;
+            box-shadow: 0px 0px 14px 0.8px #3474dbb2;
+          }
+          &:placeholder-shown {
+            letter-spacing: var(--spacing);
           }
         }
       }
@@ -64,6 +68,6 @@ export const Wrapper = styled.div`
   }
 
   .footer {
-    margin-top: 3em;
+    margin-top: 2em;
   }
 `
